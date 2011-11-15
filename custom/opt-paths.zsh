de01b6a2d31c2d80b5e8d6c52509556c2979d87c
John Reese <john@noswap.com>
2011-11-15 13:07:28 -0800

    Add subdirectories of ~/opt to $path

---
+++ custom/opt-paths.zsh
@@ -0,0 +1,17 @@
+# Automatically add subdirectories from ~/opt to the user's path.
+
+for optdir in `ls ~/opt/`
+do
+	optdirpath=~/opt/$optdir
+	if [ -d "$optdirpath" ]
+	then
+		optdirsubpath=$optdirpath/bin
+		if [ -d "$optdirsubpath" ]
+		then
+			export PATH=$PATH:$optdirsubpath
+		else
+			export PATH=$PATH:$optdirpath
+		fi
+	fi
+done
+
