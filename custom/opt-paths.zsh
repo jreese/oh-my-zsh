17918b6fb4fe02a6baab7dd8df0c7575c7d71f07
John Reese <johnr@ea2d.com>
2012-01-31 11:05:07 -0800

    Don't error when ~/opt doesn't exist

--- custom/opt-paths.zsh
+++ custom/opt-paths.zsh
@@ -1,17 +1,20 @@
 # Automatically add subdirectories from ~/opt to the user's path.
 
-for optdir in `ls ~/opt/`
-do
-	optdirpath=~/opt/$optdir
-	if [ -d "$optdirpath" ]
-	then
-		optdirsubpath=$optdirpath/bin
-		if [ -d "$optdirsubpath" ]
+if [ -d '~/opt' ]
+then
+	for optdir in `ls ~/opt/`
+	do
+		optdirpath=~/opt/$optdir
+		if [ -d "$optdirpath" ]
 		then
-			export PATH=$PATH:$optdirsubpath
-		else
-			export PATH=$PATH:$optdirpath
+			optdirsubpath=$optdirpath/bin
+			if [ -d "$optdirsubpath" ]
+			then
+				export PATH=$PATH:$optdirsubpath
+			else
+				export PATH=$PATH:$optdirpath
+			fi
 		fi
-	fi
-done
+	done
+fi
 
