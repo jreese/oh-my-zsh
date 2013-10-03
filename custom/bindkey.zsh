d198195b3d730cf8bf2663fb328b1d742cbe2468
John Reese <john@noswap.com>
2013-10-03 15:06:37 -0700

    Add keybindings that make vi mode nicer

---
+++ custom/bindkey.zsh
@@ -0,0 +1,9 @@
+
+# vi-mode re-bindings
+
+bindkey '^r' history-incremental-search-backward
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+bindkey '^w' backward-kill-word
+bindkey '^k' kill-line
+bindkey '^u' backward-kill-line
