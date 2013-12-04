5b25b2ce8237a64f843daa8088a5b2eba0a30f1f
John Reese <john@noswap.com>
2013-12-03 17:46:07 -0800

    Add more bindings for home/end/ctrl-left/right

--- custom/gnome-terminal.zsh
+++ custom/gnome-terminal.zsh
@@ -12,9 +12,13 @@ bindkey '[3~' delete-char
 # Home/End
 bindkey 'OH' beginning-of-line
 bindkey 'OF' end-of-line
+bindkey '[1~' beginning-of-line
+bindkey '[4~' end-of-line
 
 # Ctrl Left/Right (^[[1;5D & ^[[1;5C)
 bindkey '[1;5D' backward-word
 bindkey '[1;5C' forward-word
+bindkey 'OD' backward-word
+bindkey 'OC' forward-word
 
 
