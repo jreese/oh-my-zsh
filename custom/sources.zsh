51f45ab4693714d79404ef1e610cc6623917dc96
John Reese <john@noswap.com>
2014-02-11 17:41:42 -0800

    Import local aliases

--- custom/sources.zsh
+++ custom/sources.zsh
@@ -8,6 +8,9 @@ fi
 if [ -f ${HOME}/.alias ]; then
 	source ${HOME}/.alias
 fi
+if [ -f ${HOME}/.alias_local ]; then
+	source ${HOME}/.alias_local
+fi
 
 # System Specific zshrc
 if [ -f ${HOME}/.zsh_local ]; then
