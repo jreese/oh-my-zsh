9ac0528380754773dcfa5d073d9af881239c5794
John Reese <john@noswap.com>
2014-08-28 16:28:15 -0700

    Remove xterm titles, and always send screen titles

--- custom/screen-titles.zsh
+++ custom/screen-titles.zsh
@@ -4,16 +4,8 @@ function screen_title() {
   # escape '%' chars in $1, make nonprintables visible
   a=${(V)1//\%/\%\%}
 
-  case $TERM in
-  screen*)
-    a=$(print -Pn "%20>...>$a" | tr -d "\n")
-    print -Pn "\ek$a:$3\e\\"      # screen title (in ^A")
-    ;;
-  xterm*|rxvt)
-    a=$(print -Pn "%40>...>$a" | tr -d "\n")
-    print -Pn "\e]2;$2 | $a:$3\a" # plain xterm title
-    ;;
-  esac
+  a=$(print -Pn "%20>...>$a" | tr -d "\n")
+  print -Pn "\ek$a:$3\e\\"      # screen title (in ^A")
 }
 
 # precmd is called just before the prompt is printed
